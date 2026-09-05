9578cc836e1119e8d0bdd62ec07d493f85518c39
Nemanja Stefanovic <nemanja.stef92@gmail.com>
2026-09-05 02:33:06 +0200

    wip

--- init-env.sh
+++ init-env.sh
@@ -1,15 +1,30 @@
-#1/bin/bash
-
+#!/bin/bash
 set -e
+cd ..
 
-echo "Clone git-repo..."
-git clone https://gerrit.googlesource.com/git-repo
-echo "repo init..."
-./git-repo/repo init -u https://github.com/n35tef/oe-manifest-odyssey.git 
-  -b scarthgap
+if [ ! -d "git-repo" ]; then
+    echo "Clone git-repo..."
+    git clone https://gerrit.googlesource.com/git-repo
+else
+    echo "git-repo already exists, skipping clone"
+fi
+
+if [ ! -d ".repo" ]; then
+    echo "repo init..."
+    ./git-repo/repo init -u https://github.com/n35tef/oe-manifest-odyssey.git -b scarthgap
+else
+    echo "repo already initialized, skipping init"
+fi
+
 echo "repo sync..."
 ./git-repo/repo sync
-echo "Create symlink to setup env script"
-ln -s layers/meta-st/scripts/envsetup.sh envsetup.sh
+
+if [ ! -L "envsetup.sh" ]; then
+    echo "Create symlink to setup env script"
+    ln -s layers/meta-st/scripts/envsetup.sh envsetup.sh
+else
+    echo "envsetup.sh symlink already exists, skipping"
+fi
+
 echo "Source setup env script"
-yes | DISTRO=openstlinux-weston MACHINE=stm32mp1 source layers/meta-st/scripts/envsetup.sh --pkg-update --no-ui
+DISTRO=openstlinux-weston MACHINE=stm32mp1 source layers/meta-st/scripts/envsetup.sh --pkg-update --no-ui
